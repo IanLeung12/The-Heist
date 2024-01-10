558c763dd9e12f2da24b49ac938a2c4c574a50c5
toxicface <47838537+toxicface@users.noreply.github.com>
2024-01-10 09:25:27 -0500

    text based game

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,49 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Visualizer visualizer = new Visualizer();
-        while (true) {
-            visualizer.refresh();
-            Thread.sleep(15);
+//        Visualizer visualizer = new Visualizer();
+//        while (true) {
+//            visualizer.refresh();
+//            Thread.sleep(15);
+//        }
+
+        ArrayList<Integer> deck = new ArrayList<>(13);
+        deck.add(-1);
+        for (int i = 1; i < 13; i ++) {
+            deck.add(i);
         }
+
+        boolean inPlay = true;
+        int sum = 0;
+        Scanner input = new Scanner(System.in);
+        while (inPlay) {
+            int pos = (int) (Math.random() * deck.size());
+            int pull = deck.remove(pos);
+            if (pull == -1) {
+                inPlay = false;
+                sum = 0;
+                System.out.println("You pulled a bomb and died.");
+            } else {
+                sum += pull;
+                System.out.println("You pulled a " + pull + ".");
+                System.out.println("Total Sum: " + sum);
+                System.out.println("Cards left: " + deck.size());
+                System.out.println("Do you wish to keep playing? (y/n)");
+                if (!input.next().equals("y")) {
+                    inPlay = false;
+                }
+            }
+        }
+
+        System.out.println("You left with $" + sum);
+
+
+
+
     }
 }
